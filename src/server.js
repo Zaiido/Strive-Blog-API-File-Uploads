c3597422de8f6094781ba42eb28299d959bc7456
Zaide Kurti <zaiido1920@gmail.com>
2023-03-07 13:33:29 +0100

    Added whitelist

--- src/server.js
+++ src/server.js
@@ -5,13 +5,23 @@ import { badRequestHandler, generalErrorHandler, notfoundHandler } from './error
 import authorsRouter from './api/authors/index.js'
 import postsRouter from './api/blogPosts/index.js'
 import { join } from 'path'
+import createHttpError from 'http-errors'
 
 
 const server = Express()
 const port = process.env.PORT
 const publicFolderPath = join(process.cwd(), "./public")
 
-server.use(cors())
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL]
+server.use(cors({
+    origin: (currentOrigin, corsNext) => {
+        if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+            corsNext(null, true)
+        } else {
+            corsNext(createHttpError(400, `Origin ${currentOrigin} is not in the whitelist!`))
+        }
+    }
+}))
 server.use(Express.static(publicFolderPath))
 server.use(Express.json())
 

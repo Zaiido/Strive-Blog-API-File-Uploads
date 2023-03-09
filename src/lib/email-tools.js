670613242ca7781e8f2b964644882d668e930f3b
Zaide Kurti <zaiido1920@gmail.com>
2023-03-09 14:32:33 +0100

    Send email

--- src/lib/email-tools.js
+++ src/lib/email-tools.js
@@ -2,13 +2,13 @@ import sgMail from "@sendgrid/mail"
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-export const sendEmailToAuthor = async recipientAdress => {
+export const sendEmailToAuthor = async recipientAddress => {
     const msg = {
-        to: recipientAdress,
+        to: recipientAddress,
         from: process.env.SENDER_EMAIL_ADDRESS,
         subject: "Yey!",
         text: "You made it!",
-        html: "<strong>Purrfect!</strong>",
+        html: "<strong>Your blog was posted!</strong>",
     }
     await sgMail.send(msg)
 }
